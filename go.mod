b98550d6b2c2ef6d84df8f938b578041d51350df
mateus-henrique-silva <henrique.s@gsuite.iff.edu.br>
2023-12-19 15:52:42 -0300

    .

--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module go.mod
 
 go 1.20
 
-require github.com/gorilla/mux v1.8.1 // indirect
+require k8s.io/apimachinery v0.29.0
+
+require (
+	github.com/go-logr/logr v1.3.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/google/gofuzz v1.2.0 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+	k8s.io/klog/v2 v2.110.1 // indirect
+)
--- middleware/balanceMiddleware.go
+++ middleware/balanceMiddleware.go
@@ -4,15 +4,32 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
+	"strconv"
+	"sync"
 
 	"go.mod/entity"
 	"go.mod/libs/rest"
 )
 
+var requestCount int
+var mu sync.Mutex
+
 func BalanceMiddleware(other http.HandlerFunc) http.HandlerFunc {
+	mu.Lock()
+	requestCount++
+	mu.Unlock()
 	return func(w http.ResponseWriter, r *http.Request) {
 		size := rest.ReadRequest(w, r.Body)
-		ur, _ := url.Parse("http://localhost:6667")
+		var ur *url.URL
+		var port string
+		if size > 10000 {
+			for i := 0; i <= requestCount; i++ {
+				n := 6666 + 1
+				strconv.Itoa(n)
+			}
+		}
+
+		ur, _ = url.Parse("http://localhost:" + port)
 		v := entity.LoadBalancer{
 			Servers: []*entity.BackendServer{
 				&entity.BackendServer{
